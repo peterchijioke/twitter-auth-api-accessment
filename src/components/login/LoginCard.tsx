66b84a3c0ac0e2fa2980d436b89e201e82a047d0
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-12-03 14:13:05 +0100

    update

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -1,34 +1,42 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "../../styles/Login.module.css";
 import Button from "../common/Button";
 import { useNavigate } from "react-router-dom";
 import { useTwitterConnection } from "@ekaruz/react-social-auth";
 import InnerCard from "../common/InnerCard";
 
-export default function LoginCard() {
-  const navigate: any = useNavigate();
+// LoginCard component for handling Twitter authentication
+const LoginCard: React.FC = () => {
+  // Destructure values from useTwitterConnection hook
   const { onTwitterConnect, isLoading, twitterData } = useTwitterConnection({
+    // Use process.env.REACT_APP_TWITTER_CLIENT_ID safely
     clientId: process.env.REACT_APP_TWITTER_CLIENT_ID as string,
     redirect_uri: "http://www.localhost:3001/oauth/twitter" as string,
     isOnlyGetCode: false,
     isOnlyGetToken: false,
+    // Provide an empty function for onReject
     onReject: () => {},
-    clientKeys: `${
-      (process.env.REACT_APP_TWITTER_CLIENT_ID +
-        ":" +
-        process.env.REACT_APP_CLIENT_SECRET) as string
-    }`,
+    // Combine client ID and client secret for clientKeys
+    clientKeys:
+      `${process.env.REACT_APP_TWITTER_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}` as string,
   });
 
   return (
+    // Render an InnerCard component to wrap the login content
     <InnerCard>
+      {/* Button for initiating Twitter authentication */}
       <Button className={styles.Button} onClick={onTwitterConnect}>
+        {/* X icon for the button */}
         <img
           className={styles.xIcon}
           src={require("../../assets/images/x.png")}
+          alt="X Icon"
         />
+        {/* Text for the login button */}
         {" Login with x"}
       </Button>
     </InnerCard>
   );
-}
+};
+
+export default LoginCard;
